e3eca58367f2014d97ab94d51766d4cd77729c63
Jaret Wright <hkkA223>
2019-10-11 09:49:24 -0400

    added For Loop examples

---
+++ src/ForLoopChallenges.java
@@ -0,0 +1,17 @@
+public class ForLoopChallenges {
+    public static void main(String[] args)
+    {
+        System.out.printf("Challenge 1%n");
+        for (int i=2; i<=10; i = i+2)
+            System.out.println(i);
+
+        System.out.printf("Challenge 2%n");
+        for (int i=1; i<=5 ; i++)
+            System.out.println("Jaret");
+
+        System.out.printf("Challenge 3%n");
+        for (int i=1; i<=5 ; i++)
+            System.out.printf("%2d. Jaret%n", i);
+
+    }
+}
